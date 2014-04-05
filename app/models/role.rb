46ea2e73b762d316cf57f85f693822188a50092b
Brian Sisney <briansisney@gmail.com>
2014-04-05 15:28:13 -0400

    created model associations

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,5 @@
 class Role < ActiveRecord::Base
+  has_many :assignments
+  has_many :users, through: :assignments
+  has_many :clients, through: :assignments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :assignments
+  has_many :clients, through: :assignments
+  has_many :roles, through: :assignments
 end

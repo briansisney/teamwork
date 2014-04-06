a95fb068e777c825a437c19a3099c4613565df80
Brian Sisney <briansisney@gmail.com>
2014-04-06 01:24:38 -0400

    styled the assignments table

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,2 +1,19 @@
 module AssignmentsHelper
+  def row_color(end_date)
+    now = Time.now
+
+    if end_date.nil?
+      "info" 
+    elsif end_date < now + 7.days 
+      "warning"
+    elsif end_date > now + 7.days
+      "success"
+    elsif end_date < now
+      "danger"  
+    end
+  end
+
+  def readable_date(date)
+    date.strftime("%B %d, %Y") unless date.nil? 
+  end
 end

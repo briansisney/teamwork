4c51a8d27b7dd2a4a8e46905391173c8917bb4eb
Brian Sisney <briansisney@gmail.com>
2014-04-06 13:13:19 -0400

    edited assignments javascript so would run on page load and not just hard refresh

--- app/assets/javascripts/assignments.js
+++ app/assets/javascripts/assignments.js
@@ -1,7 +1,7 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function() {
+$(document).on('ready page:load', function() {
   $('.user_form_wrapper').click(function() {
     $(this).find('input:radio').prop("checked", true);
     $('.user_form_wrapper').removeClass('selected_role');
@@ -18,3 +18,22 @@ $(document).ready(function() {
     $(this).addClass('selected_role');
   });
 });
+
+
+// $(document).ready(function() {
+//   $('.user_form_wrapper').click(function() {
+//     $(this).find('input:radio').prop("checked", true);
+//     $('.user_form_wrapper').removeClass('selected_role');
+//     $(this).addClass('selected_role');
+//   });
+//   $('.client_form_wrapper').click(function() {
+//     $(this).find('input:radio').prop("checked", true);
+//     $('.client_form_wrapper').removeClass('selected_role');
+//     $(this).addClass('selected_role');
+//   });
+//   $('.role_form_wrapper').click(function() {
+//     $(this).find('input:radio').prop("checked", true);
+//     $('.role_form_wrapper').removeClass('selected_role');
+//     $(this).addClass('selected_role');
+//   });
+// });

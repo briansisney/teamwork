4111dedd6e6872e770ba45b7e564d830e860f5d4
Brian Sisney <briansisney@gmail.com>
2014-04-08 13:12:52 -0400

    renamed files

---
+++ app/assets/javascripts/clients.js
@@ -0,0 +1,10 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function(){
+    //add the Selected class to the checked radio button
+    $('input:checked').parent().addClass("selected_role");
+    //If another radio button is clicked, add the select class, and remove it from the previously selected radio
+
+});

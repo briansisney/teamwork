9965664905acb4c53a73a4d35e0bb3efdd91305e
Brian Sisney <briansisney@gmail.com>
2014-04-08 11:51:42 -0400

    changed join to include so that the active record would not be read only

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -4,5 +4,5 @@ class Assignment < ActiveRecord::Base
   belongs_to :role
   
   validates :user, :client, :role, presence: true
-  default_scope joins(:user).order('users.name')
+  default_scope includes(:user).order('users.name')
 end

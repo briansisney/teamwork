e1c826677e3ee26478850018a7c1113810942418
Brian Sisney <briansisney@gmail.com>
2014-04-08 17:00:58 -0400

    refactored assignment partial for assignment instances

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -2,9 +2,9 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(function(){
-    //add the Selected class to the checked radio button
-    $('input:checked').parent().addClass("selected_role");
-    //If another radio button is clicked, add the select class, and remove it from the previously selected radio
+// $(document).ready(function(){
+//     //add the Selected class to the checked radio button
+//     $('input:checked').parent().addClass("selected_role");
+//     //If another radio button is clicked, add the select class, and remove it from the previously selected radio
 
-});
+// });

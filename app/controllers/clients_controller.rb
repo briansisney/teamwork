975e07fcd24b5eed2e0d37451304e54e1620b37d
Brian Sisney <briansisney@gmail.com>
2014-04-10 12:37:20 -0400

    styled the new modal

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -37,7 +37,6 @@ class ClientsController < ApplicationController
       if @client.save
         format.html { redirect_to @client, notice: 'Client was successfully created.' }
         format.json { render action: 'show', status: :created, location: @client }
-        format.js { render layout: false }
       else
         format.html { render action: 'new' }
         format.json { render json: @client.errors, status: :unprocessable_entity }

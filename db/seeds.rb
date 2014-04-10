8db73b1e7992bb72241723312e9a5760d1e37927
Brian Sisney <briansisney@gmail.com>
2014-04-10 12:52:32 -0400

    changed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Role.create([{name: "Sales Planner"}, {name: "Sales Director"}, {name: "Account Planner"}, {name: "Account Director"}])
-User.create([{name: "Brian Sisney"}, {name: "Brad Pitt"}, {name: "RuPaul Charles"}, {name: "Quincy Jones"}])
-Client.create([{name: "McDonalds"}, {name: "Chipotle"}, {name: "Viacom"}, {name: "Forever 21"}])
+User.create([{name: "Eric Cartman"}, {name: "Stan Marsh"}, {name: "Kyle Broflovski"}, {name: "Kenny McCormick"}, {name: "Trey Parker"}, {name: "Matt Stone"}])
+Client.create([{name: "McDonalds"}, {name: "Chipotle"}, {name: "MTV"},{name: "NBC"}, {name: "Coca-Cola"}])
